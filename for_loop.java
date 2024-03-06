ba539c62634f3a2f44f78649273b4f20f7242397
VijayWhat <vijayrv042@gmail.com>
2024-03-06 18:29:37 +0530

    Loops Added in Java

---
+++ for_loop.java
@@ -0,0 +1,22 @@
+
+class for_loop{
+
+    public static void main(String[] args){
+
+        String[] cars = {"audi","BMW","chevrolet","Ford"};
+
+        for(String car : cars)
+            System.out.println("I like " + car);
+
+        System.out.println();
+
+        for(int i = 0; i < cars.length;i++)
+            System.out.println("I love " + cars[i]);
+
+        System.out.println();
+        
+        for(int j = 0; j < 4; j++)
+            System.out.println("I loves " + cars[j]);
+        
+    }
+}
