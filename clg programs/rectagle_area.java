3fde79aaa3ffdd77707c40e667c6facd11a12b91
VijayWhat <vijayrv042@gmail.com>
2024-06-27 17:40:47 +0530

    Changes in Rectangle program

--- clg programs/rectagle_area.java
+++ clg programs/rectagle_area.java
@@ -1,32 +1,23 @@
-class rect{
-    int length,width;
-    void getdata(int x,int y){
-        length=x;
-        width=y;
-    }
-    int area(){
-        int Area = length * width;
-        return Area; 
-    }
-}
-
+import java.util.*;
 
 public class rectagle_area {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        
+        rect Calcrect = new rect();
 
-        int area1, area2;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        Calcrect.length = s.nextInt();
 
-        rect rect1 = new rect();
-        rect1.length = 15;
-        rect1.width = 10;
-        area1 = rect1.length * rect1.width;
-        System.out.println("Area of Rectangle1: " + area1);
+        System.out.print("Enter the Width of the Rectangle: ");
+        Calcrect.width = s.nextInt();
 
-        rect rect2 = new rect();
-        rect2.getdata(10, 25);
-        area2 = rect2.area();
-        System.out.println("Area of Rectangle2: " + area2);
+        Calcrect.getdata(Calcrect.length, Calcrect.width);
+        int area = Calcrect.area();
+
+        System.out.println("The area of the rectangle is: " + area);
+    
     }
     
 }
