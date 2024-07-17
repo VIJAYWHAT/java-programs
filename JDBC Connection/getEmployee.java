35ccdf7400cbdc5faa4fea6e2eb2a95ab70fa40a
VijayWhat <vijayrv042@gmail.com>
2024-07-17 22:07:59 +0530

    select query added

---
+++ JDBC Connection/getEmployee.java
@@ -0,0 +1,76 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import model.Employee;
+
+public class getEmployee {
+     public static void main(String[] args) throws SQLException {
+        // Create DS
+        DataSource dataSource = getDatasource();
+
+        Employee employee = new Employee();
+        employee.setName("Venkad");
+        employee.setPosition("QA");
+
+        long EmployeeId = CreateEmployee(dataSource, employee);
+        System.out.println("Success Inserted " + EmployeeId + " Records");
+
+        Employee employees = getEmployee(dataSource, EmployeeId);
+        System.out.println(employees);
+//       List<Employee> employees = getEmployees(dataSource);
+
+
+//        System.out.println(employees);
+
+    }
+
+    private static Employee getEmployee(DataSource dataSource, Long id) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Employee employee = new Employee();
+
+        String query = "SELECT id, name, position from employee WHERE id = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        preparedStatement.setLong(1,id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if(resultSet.next()){
+            getFromResultSet(employee, resultSet);
+        }
+        return employee;
+    }
+
+
+    private static List<Employee> getEmployees(DataSource dataSource) throws SQLException {
+        Connection connection = dataSource.getConnection();
+
+        String query = "SELECT id, name, position from employee";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Employee> employees = new ArrayList<>();
+
+        while(resultSet.next()){
+            Employee employee = new Employee();
+            getFromResultSet(employee, resultSet);
+
+            employees.add(employee);
+        }
+        return employees;
+    }
+
+    private static void getFromResultSet(Employee employee, ResultSet resultSet) throws SQLException {
+        employee.setId(resultSet.getLong(1));
+        employee.setName(resultSet.getString(2));
+        employee.setPosition(resultSet.getString(3));
+    }
+}
