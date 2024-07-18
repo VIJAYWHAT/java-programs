9468f76142cf87bd91482dce0024e1c68a21874b
Vijay Raja <89501520+VIJAYWHAT@users.noreply.github.com>
2024-07-18 19:18:21 +0530

    Update HelloJdbc.java

--- JDBC Connection/HelloJdbc.java
+++ JDBC Connection/HelloJdbc.java
@@ -1,82 +1,40 @@
-import model.Employee;
+package com.techatpark.practices;
+
+import com.techatpark.practices.model.Employee;
+import com.techatpark.practices.repository.EmployeeRepository;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 
 public class HelloJdbc {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         // Create DS
         DataSource dataSource = getDatasource();
 
+        EmployeeRepository employeeRepository = new EmployeeRepository(dataSource);
         Employee employee = new Employee();
-        employee.setName("Ram");
+
+//        employee.setId(24L);
+        employee.setName("Venkad");
         employee.setPosition("QA");
-
-        long recortInserted = CreatEmployee(dataSource, employee);
-        System.out.println("Success Inserted " + recortInserted + " Records");
-
-        employee = new Employee();
-
-        employee.setName("Ramachandran");
-        employee.setPosition("PHP Dev");
-
-//        int resultupdate = UpdateEmployee(dataSource, employee);
-//        System.out.println("Update Success " + resultupdate + " Rows");
-
-        employee.setId(3L);
-        Long idToDelete = employee.getId();
-
-//        int deletedRecords = DeleteEmployee(dataSource, idToDelete);
-//        System.out.println("Success Deleted " + deletedRecords + " Rows");
-
-    }
-
-    private static int DeleteEmployee(DataSource dataSource, Long idToDelete) throws SQLException {
-        String query = "DELETE FROM employee WHERE id = ?";
-
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-        preparedStatement.setLong(1, idToDelete);
-        return preparedStatement.executeUpdate();
-    }
-
-    private static int UpdateEmployee(DataSource dataSource, Employee employee) throws SQLException {
-        String query = "UPDATE employee set \"name\" = ?, \"position\" = ? WHERE id = ?";
-
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-        preparedStatement.setString(1, employee.getName());
-        preparedStatement.setString(2, employee.getPosition());
-        preparedStatement.setLong(3, employee.getId());
-        return preparedStatement.executeUpdate();
-    }
-
-    private static long CreatEmployee(DataSource dataSource, Employee employee) throws SQLException {
-        String query = "INSERT INTO employee (\"name\", \"position\") VALUES (?, ?)";
-
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-
-        preparedStatement.setString(1, employee.getName());
-        preparedStatement.setString(2, employee.getPosition());
-
-
-        long recordInserted = preparedStatement.executeUpdate();
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-
-        if(resultSet.next()){
-            recordInserted = resultSet.getLong(1);
+//        Employee employee1 = employeeRepository.save(employee);
+//        System.out.println(employee1);
+        Iterable<Employee> employees = employeeRepository.findAll();
+        for(Employee employee1 : employees){
+            System.out.println(employee1);
         }
 
-        return recordInserted;
 
+        long employeeCount = employeeRepository.count();
+        System.out.println("Employees Count: " + employeeCount);
     }
-
-
     private static DataSource getDatasource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUser("postgres");
