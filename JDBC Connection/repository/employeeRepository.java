45bb6ce5fdf4959c001b3e5f9516162d6c95d294
VijayWhat <vijayrv042@gmail.com>
2024-07-18 19:07:51 +0530

    Employee method skeleton

--- JDBC Connection/repository/employeeRepository.java
+++ JDBC Connection/repository/employeeRepository.java
@@ -1,5 +1,77 @@
-package repository;
+package com.techatpark.practices.repository;
 
-public class employeeRepository {
-    
+import com.techatpark.practices.model.Employee;
+import org.springframework.data.repository.*;
+
+import javax.sql.DataSource;
+import java.util.Optional;
+
+public class EmployeeRepository implements CrudRepository<Employee, Long> {
+
+    private final DataSource dataSource;
+
+    public EmployeeRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Override
+    public <S extends Employee> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Employee> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Employee> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Employee> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Employee entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Employee> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
