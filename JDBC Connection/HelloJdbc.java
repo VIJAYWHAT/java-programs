f11c079c98c32b90f48c998e96823deeffc8f6a5
Vijay Raja <89501520+VIJAYWHAT@users.noreply.github.com>
2024-07-19 15:31:41 +0530

    Update HelloJdbc.java

--- JDBC Connection/HelloJdbc.java
+++ JDBC Connection/HelloJdbc.java
@@ -24,16 +24,36 @@ public class HelloJdbc {
 //        employee.setId(24L);
         employee.setName("Venkad");
         employee.setPosition("QA");
-//        Employee employee1 = employeeRepository.save(employee);
-//        System.out.println(employee1);
-        Iterable<Employee> employees = employeeRepository.findAll();
+
+       /* Iterable<Employee> employees = employeeRepository.findAll();
         for(Employee employee1 : employees){
             System.out.println(employee1);
+        }*/
+
+        List<Long> employees = new ArrayList<>();
+        employee.setId(30L);
+        employees.add(employee.getId());
+        employee.setId(31L);
+        employees.add(employee.getId());
+        employee.setId(32L);
+        employees.add(employee.getId());
+
+
+//        Iterable<Employee> employeeRes = employeeRepository.findAllById(employees);
+//        System.out.println(employeeRes);
+
+//        employee.setId(30L);
+        boolean exists = employeeRepository.existsById(employee.getId());
+        if(exists){
+            System.out.println("Employee already exist");
+        }
+        else {
+            System.out.println("Employee not exist");
         }
 
 
-        long employeeCount = employeeRepository.count();
-        System.out.println("Employees Count: " + employeeCount);
+//        long employeeCount = employeeRepository.count();
+//        System.out.println("Employees Count: " + employeeCount);
     }
     private static DataSource getDatasource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
