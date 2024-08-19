9fd2dd5eb01eca947428dc6f93586533cef31c7a
VijayWhat <vijayrv042@gmail.com>
2024-08-19 13:45:39 +0530

    super variable program added

---
+++ supervariable.java
@@ -0,0 +1,21 @@
+class car{
+    String color = "white";
+}
+
+class chevrolet extends car{
+    String color = "black";
+
+    void printColor(){
+        System.out.println("Default Car Color: " + super.color);
+        System.out.println("Chevrolet Color: " + color);
+    }
+
+}
+
+class supervariable{
+
+    public static void main(String[] args){
+        chevrolet c = new chevrolet();
+        c.printColor();
+    }
+}
