c1e8111895dbbc74dad186dcae0c9ddd9c8c8cda
VijayWhat <vijayrv042@gmail.com>
2024-08-21 15:45:39 +0530

    super method program created

---
+++ supermethod.java
@@ -0,0 +1,24 @@
+class animal{
+    void eat(){
+        System.out.println("Eating...");
+    }
+} 
+class cat extends animal{
+    void eat(){ System.out.println("Eating Fish..."); }
+    void sleep(){ System.out.println("Sleeping..."); }
+
+    void work(){
+        super.eat();
+        sleep();
+    }
+}
+
+
+public class supermethod {
+    public static void main(String... args){
+
+        cat c = new cat();
+        c.work();
+        
+    }
+}
