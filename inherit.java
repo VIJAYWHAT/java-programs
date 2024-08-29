3dd4905a2a2e91c528c005ccd87e0fdc63be54ca
VijayWhat <vijayrv042@gmail.com>
2024-08-29 18:30:47 +0530

    inheritance program added

--- inherit.java
+++ inherit.java
@@ -1,3 +1,36 @@
+// Base or Parent Class
+class Bicycle {
+
+    public int gear;
+    public int speed;
+
+    // Bicycle constructor
+    public Bicycle(int gear, int speed){
+        this.gear = gear;
+        this.speed = speed;
+    }
+
+    public String toString(){
+        return " No of Gears = " + gear + "\n Speed of the bicycle = " + speed;
+    }
+}
+
+class MountainBike extends Bicycle {
+    public int seatHeight;
+
+    public MountainBike(int gear, int speed, int seatHeight){
+        super(gear, speed);
+        this.seatHeight = seatHeight;
+    }
+    public void setHeight(int newValue){ seatHeight = newValue; }
+
+    @Override public String toString(){
+        return super.toString() + "\n Seat Height = " + seatHeight;
+    }
+} 
 public class inherit {
-    
+    public static void main(String... args){
+        MountainBike mb = new MountainBike(4, 100, 25);
+        System.out.println(mb.toString());
+    }
 }
