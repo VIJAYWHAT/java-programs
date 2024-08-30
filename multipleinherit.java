c5f2493cba8536b3d5815cacfd56a9bb9c065b77
VijayWhat <vijayrv042@gmail.com>
2024-08-30 18:09:47 +0530

    multiple inheritance implementing...

---
+++ multipleinherit.java
@@ -0,0 +1,28 @@
+interface one {
+    String printname();
+}
+interface two {
+    void printdept();
+}
+interface three extends one, two {
+    void printblock();
+}
+
+class vj implements three {
+    @Override public void printdept(){
+        System.out.println("Computer science");
+    }
+    public void printblock(){
+        System.out.println("Block 1");
+    }
+    public String printname(){
+        return "Vijay";
+    }
+}
+
+public class multipleinherit {
+    public static void main(String... args){
+        vj v = new vj();
+        System.out.print("Name: " + v.printname());
+    }
+}
