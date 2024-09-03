b604c5d6c01f4020973c8b9a71031b32efae7d3e
VijayWhat <vijayrv042@gmail.com>
2024-09-03 15:53:43 +0530

    inner abstract class added

---
+++ innerAbstract.java
@@ -0,0 +1,26 @@
+abstract class A {
+    abstract class B {
+
+        abstract void myMethod();
+    }
+} 
+
+class C extends A {
+    class D extends B {
+        void myMethod() {
+            System.out.println("My method was implemented in the Class D...");
+        }
+    }
+}
+
+public class innerAbstract {
+    
+    public static void main(String... args) {
+        
+        C outer = new C();
+        C.D inner = outer.new D();
+
+        inner.myMethod();
+
+    }
+}
