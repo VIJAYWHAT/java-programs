8035240bbb445fea45174ad8681947f028abcaaf
VijayWhat <vijayrv042@gmail.com>
2024-09-03 18:45:44 +0530

    cahnges

---
+++ OPPS/polymorphism2.java
@@ -0,0 +1,14 @@
+package OPPS;
+class Parent {
+    void print() {
+        System.out.println("Parent Class");
+    }
+}
+class child1 extends Parent {
+    void print() {
+        System.out.println("Child1 Class");
+    }
+}
+public class polymorphism2 {
+    
+}
