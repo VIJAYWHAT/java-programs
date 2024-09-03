17b7d3b00feb7e2aba7548c05f3e4055730d5ca0
VijayWhat <vijayrv042@gmail.com>
2024-09-03 16:35:56 +0530

    encapsulation program added

---
+++ encapsulation.java
@@ -0,0 +1,27 @@
+class person {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() { return name; }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() { return age; }
+}
+
+public class encapsulation {
+    public static void main(String[] args) {
+
+        person vj = new person();
+        vj.setName("Vijay R");
+        vj.setAge(19);
+
+        System.out.println("Name: " + vj.getName());
+        System.out.println("Age: " + vj.getAge());
+
+    }
+}
