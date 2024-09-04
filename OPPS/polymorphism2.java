6c637f80b68be69898ffdb4dd3b85dbbe0ad0188
VijayWhat <vijayrv042@gmail.com>
2024-09-04 18:16:31 +0530

    changes

--- OPPS/polymorphism2.java
+++ OPPS/polymorphism2.java
@@ -6,9 +6,25 @@ class Parent {
 }
 class child1 extends Parent {
     void print() {
-        System.out.println("Child1 Class");
+        System.out.println("Child Class1");
+    }
+}
+
+class child2 extends Parent {
+    void print() {
+        System.out.println("Child class2");
     }
 }
 public class polymorphism2 {
+
+    public static void main(String[] args) {
+        Parent p;
+
+        p = new child1();
+        p.print();
+
+        p = new child2();
+        p.print();
+    }
     
 }
