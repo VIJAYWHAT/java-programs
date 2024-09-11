d956ccf7a44cc70e3637611456e934563e80e5e8
VijayWhat <vijayrv042@gmail.com>
2024-09-11 18:30:17 +0530

    changes

--- RandomStringGenerator.java
+++ RandomStringGenerator.java
@@ -8,18 +8,15 @@ public class RandomStringGenerator {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
-            System.out.println("i : " + i);
             int index = random.nextInt(characters.length());
-            System.out.println("index: " + index);
-            System.out.println("character: " +characters.charAt(index));
             result.append(characters.charAt(index));
-            System.out.println("result: " + result);
         }
+
         return result.toString();
     }
 
     public static void main(String[] args) {
-        String randomString = getRandomString(10);
+        String randomString = getRandomString(6);
         System.out.println("Random String: " + randomString);
     }
 }
