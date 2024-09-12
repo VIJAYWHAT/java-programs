0fcb6376d7d1f6f9240969c8878cceaa5ccdb736
VijayWhat <vijayrv042@gmail.com>
2024-09-12 15:55:57 +0530

    changes in program

--- captcha.java
+++ captcha.java
@@ -4,22 +4,26 @@ public class captcha {
 
     public static void main(String[] args) {
         
-
         RandomStringGenerator random = new RandomStringGenerator();
         Scanner sc = new Scanner(System.in); 
+        boolean isCaptchaCorrect = false;
 
-        String Captcha = random.getRandomString(6);
-        
-        System.out.println("captcha: " + Captcha);
+        while (!isCaptchaCorrect) {
+            
+            String Captcha = random.getRandomString(6);
+            
+            System.out.println("captcha: " + Captcha);
 
-        System.out.print("Enter Captcha: ");
-        String valid = sc.nextLine();
+            System.out.print("Enter Captcha: ");
+            String valid = sc.nextLine();
 
-        if (valid.equals(Captcha)) {
-            System.out.println("Captcha is correct!");
-        }
-        else{
-            System.out.println("Incorrect captcha! Try again");
+            if (valid.equals(Captcha)) {
+                System.out.println("Captcha is correct!");
+                isCaptchaCorrect = true;
+            }
+            else{
+                System.out.println("Incorrect captcha! Try again");
+            }
         }
 
     }
