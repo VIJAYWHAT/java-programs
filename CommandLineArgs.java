c8736337b4e19fed029b6ca16d6920d5ee0c55e3
VijayWhat <vijayrv042@gmail.com>
2024-09-12 16:02:23 +0530

    changes

--- CommandLineArgs.java
+++ CommandLineArgs.java
@@ -2,18 +2,18 @@ public class CommandLineArgs{
 
         public static void main(String[] args){
 
-                String Name = args[0];
+        String Name = args[0];
 
-                System.out.println("Name: " + Name);
+        System.out.println("Name: " + Name);
 
-                int ArgsLength = args.length;
+        int ArgsLength = args.length;
 
-                if(ArgsLength > 1){
+        if(ArgsLength > 1){
 
-                        String FirstArgs = args[0];
-                        System.out.println("First Name: " + FirstArgs);
+                String FirstArgs = args[0];
+                System.out.println("First Name: " + FirstArgs);
 
-                        String LastArgs = args[ArgsLength-1];
+                String LastArgs = args[ArgsLength-1];
                         System.out.println("Last Name: "+ LastArgs );
                         
                 }
