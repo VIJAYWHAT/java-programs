310d14c8a18b17e4752eb423259bfb362fd452d2
VijayWhat <vijayrv042@gmail.com>
2024-09-12 16:43:34 +0530

    changes in array program

--- classInArray.java
+++ classInArray.java
@@ -1,22 +1,27 @@
-class student{
+class student {
     public int roll_no;
     public String name;
-    student(int roll_no,String name) {
+
+    student(int roll_no, String name) {
         this.roll_no = roll_no;
         this.name = name;
     }
 }
+
 public class classInArray {
     public static void main(String[] args) {
-        student[] arr = new student[3];
+        student[] arr = new student[5];
 
         arr[0] = new student(1, "Vijay");
         arr[1] = new student(2, "Ram");
         arr[2] = new student(3, "Gopi");
 
-        for(int i = 0; i < arr.length; i++){
-            System.out.println("Students : " + i + " : " + arr[i].roll_no + " - " + arr[i].name);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) { // Check if the element is not null
+                System.out.println("Student " + i + " : " + arr[i].roll_no + " - " + arr[i].name);
+            } else {
+                System.out.println("Student " + i + " : Not assigned yet.");
+            }
         }
-
     }
 }
