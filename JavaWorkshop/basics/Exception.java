43aec1cb25c403c46b351afd02bdf52dc21fda1a
VijayWhat <vijayrv042@gmail.com>
2024-10-03 16:20:20 +0530

    Exception program added

---
+++ JavaWorkshop/basics/Exception.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Exception {
+    public static void main(String[] args) {
+    myMethod();
+    
+}
+
+    private static void myMethod() {
+        try {
+            List list = (List) Class.forName("com.techatpark.practices.oops.HelloClass").newInstance();
+            list.add("VJ");
+            System.out.println(list);
+        }
+        catch(ClassNotFoundException cnf){
+            System.out.println("Class ah kanam...");
+        } catch (InstantiationException e) {
+            System.out.println("Instance create panna mutiyalaa...");
+        } catch (IllegalAccessException e) {
+            System.out.println("You are under arrest!");
+        }
+    }
+}
