6d6254497c58de7d9cdaa4a451a26e7a080aafdb
VijayWhat <vijayrv042@gmail.com>
2024-10-08 16:26:32 +0530

    generics programs added

---
+++ JavaWorkshop/generics/Pair.java
@@ -0,0 +1,25 @@
+package generics;
+
+public class Pair <K, V> {
+    K key;
+    V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+
+
+}
