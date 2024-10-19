0359b726032d657859f2ed886ef842771cdb673a
VijayWhat <vijayrv042@gmail.com>
2024-10-19 16:03:05 +0530

    thread program added

---
+++ clg_programs/TreadTest.java
@@ -0,0 +1,47 @@
+class newthread implements Runnable
+{
+Thread t;
+newthread()
+{
+t=new Thread(this,"Demothread");
+System.out.println("child thread"+t);
+t.start();
+}
+public void run()
+{
+try
+{
+for(int i=5;i>0;i--)
+{
+System.out.println("child thread"+i);
+Thread.sleep(500);
+}
+}
+catch(InterruptedException e)
+{
+System.out.println("child interrupted");
+}
+System.out.println("exit child thread");
+}
+}
+
+public class TreadTest
+{
+public static void main(String ab[])
+{
+new newthread();
+try
+{
+for(int i=5;i>0;i--)
+{
+System.out.println("main thread"+i);
+Thread.sleep(1000);
+}
+}
+catch(InterruptedException e)
+{
+System.out.println("main Thread interrupted");
+}
+System.out.println("main thread exit");
+}
+}
