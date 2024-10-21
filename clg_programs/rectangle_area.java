962a4e0c54c0b7188f12d083b8b839a519843039
VijayWhat <vijayrv042@gmail.com>
2024-10-21 20:20:20 +0530

    rectangle area program added

--- clg_programs/rectangle_area.java
+++ clg_programs/rectangle_area.java
@@ -1,26 +1,31 @@
 package clg_programs;
-import java.util.*;
-
-public class rectangle_area {
-
-    public static void main(String[] args) {
-        
-        rect_area Calcrect = new rect_area();
-
-        try(Scanner s = new Scanner(System.in);){
-            System.out.print("Enter the length of the rectangle: ");
-            Calcrect.length = s.nextInt();
     
-            System.out.print("Enter the Width of the Rectangle: ");
-            Calcrect.width = s.nextInt();
-    
-            Calcrect.getdata(Calcrect.length, Calcrect.width);
-            int area = Calcrect.area();
-            System.out.println("The area of the rectangle is: " + area);
-        }
-        
-
-    
+class rectangle {
+        int length,width;
+        void getdata (int x,int y) {
+            length = x;
+            width = y;
     }
-    
+    int rectarea() {
+        int area=length*width;
+        return(area);
+    }
+}
+class rectangle_area {
+    public static void main(String ab[]) {
+        
+        int a1,a2;
+        rectangle rect1=new rectangle();
+        rectangle rect2=new rectangle();
+        
+        rect1.length=5;
+        rect1.width=10;
+        a1=rect1.length*rect1.width;
+        
+        rect2.getdata(10,10);
+        a2=rect2.rectarea();
+        
+        System.out.println("area1 = " + a1);
+        System.out.println("area2 = " + a2);
+    }
 }
