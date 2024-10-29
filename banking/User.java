a66af40ea38d3613a2e7155d40184d029888a279
VijayWhat <vijayrv042@gmail.com>
2024-10-29 15:43:09 +0530

    Banking app initialize

---
+++ banking/User.java
@@ -0,0 +1,5 @@
+package banking;
+
+public class User {
+
+}
---
+++ banking/bankApp.java
@@ -0,0 +1,7 @@
+package banking;
+
+public class bankApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Bank App");
+    }
+}
