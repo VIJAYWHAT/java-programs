a60d895a3a6359adad69f51bc84a256c4bef6e23
VijayWhat <vijayrv042@gmail.com>
2024-10-29 15:45:30 +0530

    User class added

--- banking/User.java
+++ banking/User.java
@@ -1,5 +1,20 @@
 package banking;
+import java.io.Serializable;
 
-public class User {
+public class User implements Serializable {
 
+    private int accountNo;
+    private String userName;
+    private String password;
+
+    public User(int accountNo, String userName, String password) {
+        this.accountNo = accountNo;
+        this.userName = userName;
+        this.password = password;
+    }
+    
+    public int getAccountNo() { return accountNo; }
+    public String getUserName() { return userName; }
+    public String getPassword() { return password; }
+    
 }
