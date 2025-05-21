d41e1a1711075f11a6961b368ca3f2eec3617d6e
VijayWhat <vijayrv042@gmail.com>
2025-05-21 23:45:18 +0530

    update pattern

--- oops/CreationalMagic.java
+++ oops/CreationalMagic.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collection;
 
+// This code demonstrates a simple example of a composite pattern where
+// an Item can be a simple item or a box that can contain other items.
 public class CreationalMagic {
     public static void main(String[] args) {
         Item pen = new Item();
